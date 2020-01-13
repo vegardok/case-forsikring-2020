6c07929f55cf3a1271339c689d7bad419893f0f8
Vegard Økland <vegard.okland@schibsted.com>
2020-01-13 20:43:25 +0100

    Setup css/normalize/font

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
 import React from 'react';
+/* eslint no-unused-vars: 0 */
+import normalize from 'normalize.css';
+import css from '../assets/style.css';
+
+
 import Header from '../components/Header.js';
 import InsuranceForm from '../components/InsuranceForm.js';
 
