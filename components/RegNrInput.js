fd14ad985575a865d003e80d59445b1403df504d
Vegard Økland <vegard.okland@schibsted.com>
2020-01-13 19:37:01 +0100

    Add reg nr input

---
+++ components/RegNrInput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+// TODO: reg nr validation client or server side?
+
+const RegNrInput = () => (
+  <label>
+    Bilens registreringsnummer
+    <input type="text" placeholder="e.g. AB 12345" />
+  </label>
+);
+
+
+export default RegNrInput;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Header from '../components/Header.js';
+import RegNrInput from '../components/RegNrInput.js';
 
 const Index = () => (
-  <Header />
+  <>
+    <Header />
+    <form>
+      <RegNrInput />
+    </form>
+  </>
 );
 
 export default Index;
