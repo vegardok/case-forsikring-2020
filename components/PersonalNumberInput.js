108135998988b0238f3b8b303ad487190c28ca3d
Vegard Økland <vegard.okland@schibsted.com>
2020-01-14 16:49:33 +0100

    Add personal number redux setup

--- components/PersonalNumberInput.js
+++ components/PersonalNumberInput.js
@@ -1,15 +1,76 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
+const INPUT_CHANGE = 'PersonalNumber/CHANGE';
+const INPUT_INTERACTION = 'PersonalNumber/INTERACTION';
+
+const defaultState = {
+  nr: undefined,
+  interactedWith: false,
+};
+
+export const reducer = (previousState = defaultState, action) => {
+  switch (action.type) {
+    case INPUT_INTERACTION: {
+      return {...previousState, interactedWith: true};
+    }
+    case INPUT_CHANGE: {
+      return {...previousState, nr: action.value};
+    }
+    default: {
+      return previousState;
+    }
+  }
+};
+
+const toggleInputInteraction = () => ({
+  type: INPUT_INTERACTION,
+});
+
+const changeInput = (bonus) => ({
+  type: INPUT_CHANGE,
+  value: bonus,
+});
+
 
 // TODO: client side validation
-const PersonalNumberInput = () => (
+const PersonalNumberInput = ({
+  nr,
+  changeInput,
+  toggleInputInteraction,
+  valid,
+  showValidation,
+}) => (
   <div className="field">
     <label className="label">
       <div className="control">
         Fødselsnummer
-        <input className="input" placeholder="11 siffer" />
+        <input
+          className={['input',
+                      showValidation &&
+                      (valid ? 'is-success' : 'is-danger')].join(' ')}
+          onChange={(e) => changeInput(e.target.value)}
+          onBlur={() => toggleInputInteraction()}
+          value={nr}
+          placeholder="11 siffer"
+        />
       </div>
     </label>
   </div>
 );
 
-export default PersonalNumberInput;
+const mapStateToProps = (state, ownProps) => ({
+  bonus: state.pn.nr,
+  valid: /^[\d]{11,11}$/.test(state.pn.nr),
+  showValidation: state.pn.interactedWith && state.pn.nr.length > 0,
+});
+
+const mapDispatchToProps = {
+  toggleInputInteraction,
+  changeInput,
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(PersonalNumberInput);
--- components/reducers.js
+++ components/reducers.js
@@ -1,9 +1,10 @@
 import {combineReducers} from 'redux';
 import {reducer as regNr} from './RegNrInput';
 import {reducer as bonus} from './BonusSelector';
-
+import {reducer as pn} from './PersonalNumberInput';
 
 export default combineReducers({
   regNr,
   bonus,
+  pn,
 });
