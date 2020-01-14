e1064f091c8bdd4b16ce00812a4d0333b28adadd
Vegard Økland <vegard.okland@schibsted.com>
2020-01-14 17:04:41 +0100

    Set up redux name state

--- components/NameInputs.js
+++ components/NameInputs.js
@@ -1,23 +1,81 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
-export const FirstNameInput = () => (
+const CHANGE_FIRST_NAME = 'Name/CHANGE_FIRST_NAME';
+const CHANGE_LAST_NAME = 'Name/CHANGE_LAST_NAME';
+
+const changeFirstName = (name) => ({
+  type: CHANGE_FIRST_NAME,
+  value: name,
+});
+
+const changeLastName = (name) => ({
+  type: CHANGE_LAST_NAME,
+  value: name,
+});
+
+const defaultState = {
+  firstName: '',
+  lastName: '',
+};
+
+export const reducer = (previousState = defaultState, action) => {
+  switch (action.type) {
+    case CHANGE_FIRST_NAME: {
+      return {...previousState, firstName: action.value};
+    }
+    case CHANGE_LAST_NAME: {
+      return {...previousState, lastName: action.value};
+    }
+    default: {
+      return previousState;
+    }
+  }
+};
+
+const FirstNameInput_ = ({
+  value,
+  changeValue,
+}) => (
   <div className="field">
     <label className="label">
       <div className="control">
         Fornavn
-        <input className="input" placeholder="placeholder" />
+        <input
+          className="input"
+          value={value}
+          onChange={(e) => changeValue(e.target.value)}
+          placeholder="placeholder" />
       </div>
     </label>
   </div>
 );
 
-export const LastNameInput = () => (
+const LastNameInput_ = ({
+  value,
+  changeValue,
+}) => (
   <div className="field">
     <label className="label">
       <div className="control">
         Etternavn
-        <input className="input" placeholder="placeholder" />
+        <input
+          className="input"
+          value={value}
+          onChange={(e) => changeValue(e.target.value)}
+          placeholder="placeholder" />
       </div>
     </label>
   </div>
 );
+
+export const FirstNameInput = connect(
+    (state) => ({value: state.name.firstName}), {
+      changeValue: changeFirstName,
+    })(FirstNameInput_);
+
+
+export const LastNameInput = connect(
+    (state) => ({value: state.name.lastName}), {
+      changeValue: changeLastName,
+    })(LastNameInput_);
--- components/reducers.js
+++ components/reducers.js
@@ -2,9 +2,11 @@ import {combineReducers} from 'redux';
 import {reducer as regNr} from './RegNrInput';
 import {reducer as bonus} from './BonusSelector';
 import {reducer as pn} from './PersonalNumberInput';
+import {reducer as name} from './NameInputs';
 
 export default combineReducers({
   regNr,
   bonus,
   pn,
+  name,
 });
